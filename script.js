30bb83787224ffc92daed7ba948263f8666d8625
jackLove5 <jacklove314@hotmail.com>
2020-08-02 20:16:40 -0500

    board no longer fixed size

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ function initBoard() {
 }
 
 function initSquares() {
-  for (var row = 1; row <= 10; row++) {
-    for (var col = 1; col <= 10; col++) {
-      var obj = (typeof gameBoard.pieces[row-1][col-1]) !== 'undefined' ? gameBoard.pieces[row-1][col-1] : {};
+  for (var row = 0; row < gameBoard.size; row++) {
+    for (var col = 0; col < gameBoard.size; col++) {
+      var obj = (typeof gameBoard.pieces[row][col]) !== 'undefined' ? gameBoard.pieces[row][col] : {};
       var displayText = ('rank' in obj) ? obj.rank : '';
-      var color = ('team' in obj) ? obj.team : 'Black';
-      var divStr = "<div class=\"Piece " + "row"+row + " col"+col + " " + color + " \">" +  displayText + "</div>";
-      $("#Board").append(divStr);
+      var color = ('team' in obj) ? obj.team : '';
+      var divStr = "<div class=\"square " + "row"+row + " col"+col + " " + color + " \">" +  displayText + "</div>";
+      $("#board").append(divStr);
     }
   }
 }
 
 function updateSquares() {
-  for (var row = 1; row <= 10; row++) {
-    for (var col = 1; col <= 10; col++) {
-      var obj = (typeof gameBoard.pieces[row-1][col-1]) !== 'undefined' ? gameBoard.pieces[row-1][col-1] : {};
+  for (var row = 0; row < gameBoard.size; row++) {
+    for (var col = 0; col < gameBoard.size; col++) {
+      var obj = (typeof gameBoard.pieces[row][col]) !== 'undefined' ? gameBoard.pieces[row][col] : {};
       var displayText = ('rank' in obj) ? obj.rank : '';
       displayText = displayText == "None" ? " " : displayText;
-      var color = ('team' in obj) ? obj.team : 'Black';
+      var color = ('team' in obj) ? obj.team : '';
       $(".row" + row).filter(".col" + col).text(displayText);
       $(".row" + row).filter(".col" + col).removeClass(['Red', 'Blue', 'None']);
       $(".row" + row).filter(".col" + col).addClass(color);
@@ -39,37 +39,39 @@ function updateSquares() {
 }
 
 function setSelected(row, col) {
-  $('.Piece').each( function(index) {
-    if (row == Math.floor($(this).position().top / 60) && col == Math.floor($(this).position().left / 60)) {
-      $(this).addClass('Select');
+  $('.square').each( function(index) {
+    var width = $(this).width();
+    var height = $(this).height();
+    if (row == Math.floor($(this).position().top / height) && col == Math.floor($(this).position().left / width)) {
+      $(this).addClass('select');
     }
   });
 }
 
-function deselect(row, col) {
-  $('.Piece').each( function(index) {
-    if (row == Math.floor($(this).position().top / 60) && col == Math.floor($(this).position().left / 60)) {
-      $(this).removeClass('Select');
-    }
+function deselect() {
+  $('.square').each(function(index) {
+    $(this).removeClass('select');
   });
 }
 
-$(document).on('click', '.Piece', function(e) {
-  var boardPosition = $('#Board').position();
-  var localX = Math.floor(e.pageX - boardPosition.left);
-  var localY = Math.floor(e.pageY - boardPosition.top);
-  var row = Math.floor(localY / 60);
-  var col = Math.floor(localX / 60);
-  var obj = gameBoard.pieces[row][col];
+$(document).on('click', '.square', function(e) {
+  var boardPosition = $('#board').position();
+  var squareWidth = $('#board').width() / gameBoard.size;
+  var squareHeight = $('#board').height() / gameBoard.size;
+
+  var row = Math.floor((e.pageY - boardPosition.top) / squareHeight);
+  var col = Math.floor((e.pageX - boardPosition.left) / squareWidth);
+
+  var squareObj = gameBoard.pieces[row][col];
   if (jQuery.isEmptyObject(gameBoard.selected)) {
-    if (('team' in obj) && obj.team == 'Blue'){
+    if (('team' in squareObj) && squareObj.team == 'Blue'){
       setSelected(row, col);
       gameBoard.selected = {row: row, col: col};
     }
   }
   else {
     var dst = {row: row, col: col};
-    deselect(gameBoard.selected.row, gameBoard.selected.col);
+    deselect();
     sendMsg(gameBoard.selected, dst);
     gameBoard.selected = {};
   }
@@ -77,7 +79,6 @@ $(document).on('click', '.Piece', function(e) {
 
 const socket = io("http://localhost:8080");
 socket.on("boardUpdate", function(data) {
-  console.log(data);
   var obj = JSON.parse(data);
   gameBoard.pieces = obj.board;
   updateSquares();
@@ -93,6 +94,16 @@ function startGame() {
   socket.emit("start_game");
 }
 
+function setBoardWidth() {
+  var boardWidth = $('#board').width();
+  $('#board').css({
+    'height': boardWidth + 'px'
+  });
+}
+
+window.addEventListener('resize', setBoardWidth);
+
+setBoardWidth();
 initBoard();
 initSquares();
 socket.emit("connection");
